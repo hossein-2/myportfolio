2422f492aed93cfe78d78d1e6900c3f527c9d824
hossein tabatabaei <hosseintabatabaei@yahoo.com>
2020-01-17 00:55:58 +0330

    another commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import ToDo from "./components/ToDo";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="container mt-3">
-        <ToDo />
+    <Router basename="/myportfolio">
+      <div className="App">
+        <Header />
+        <div className="container mt-3">
+          <ToDo />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
